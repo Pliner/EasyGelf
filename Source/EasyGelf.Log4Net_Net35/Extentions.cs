3a2e3dc726acc4f2b97cdb7ea1414ca225a679fa
Yury Pliner <yury.pliner@gmail.com>
2015-03-31 22:34:37 +0500

    delete trash

--- Source/EasyGelf.Log4Net_Net35/Extentions.cs
+++
@@ -1,9 +0,0 @@
-﻿using EasyGelf.Core;
-using log4net.Core;
-
-namespace EasyGelf.Log4Net
-{
-    public static class Extentions
-    {
-    }
-}
--- Source/EasyGelf.Log4Net_Net35/Transport/Udp/UdpTransportConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace EasyGelf.Log4Net.Transport.Udp
-{
-    public sealed class UdpTransportConfiguration
-    {
-        public IPEndPoint Host { get; set; }
-    }
-}
