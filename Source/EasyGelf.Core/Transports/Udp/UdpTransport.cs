04157e3a3d7c60fa95a91db736e6c07679e9aa3d
John-Paul Dienst <jpdienst@gmail.com>
2017-08-28 23:02:58 -0400

    Update UDP

--- Source/EasyGelf.Core/Transports/Udp/UdpTransport.cs
+++ Source/EasyGelf.Core/Transports/Udp/UdpTransport.cs
@@ -1,30 +1,50 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using EasyGelf.Core.Encoders;
 
 namespace EasyGelf.Core.Transports.Udp
 {
-    public sealed class UdpTransport : ITransport
+    public sealed class UdpTransport : ITransport, IDisposable
     {
         private readonly UdpTransportConfiguration configuration;
         private readonly ITransportEncoder encoder;
         private readonly IGelfMessageSerializer messageSerializer;
+        private readonly UdpClient udpClient;
+        private bool disposed;
 
-        public UdpTransport(UdpTransportConfiguration configuration, ITransportEncoder encoder, IGelfMessageSerializer messageSerializer)
+        public UdpTransport(
+            UdpTransportConfiguration configuration,
+            ITransportEncoder encoder,
+            IGelfMessageSerializer messageSerializer)
         {
             this.configuration = configuration;
             this.encoder = encoder;
             this.messageSerializer = messageSerializer;
+            this.udpClient = new UdpClient();
         }
 
         public void Send(GelfMessage message)
         {
-            using (var udpClient = new UdpClient())
-                foreach (var bytes in encoder.Encode(messageSerializer.Serialize(message)))
-                    udpClient.Send(bytes, bytes.Length, configuration.GetHost());
+            var serialzed = messageSerializer.Serialize(message);
+            var encoded = encoder.Encode(serialzed);
+            foreach (var bytes in encoded)
+            {
+                udpClient.Send(bytes, bytes.Length, configuration.GetHost());
+            }
         }
 
         public void Close()
         {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                udpClient.Dispose();
+                disposed = true;
+            }
         }
     }
 }
