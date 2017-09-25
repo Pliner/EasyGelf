7e35cd02b639c8f858b2b38ff659fca2c15a70d8
sabarcelos <sabarcelos@gmail.com>
2017-09-25 13:34:45 -0300

    Implemented Http target to Nlog

---
+++ Source/EasyGelf.NLog/GelfHttpTarget.cs
@@ -0,0 +1,40 @@
+﻿using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasyGelf.Core;
+using EasyGelf.Core.Transports;
+using EasyGelf.Core.Transports.Http;
+
+namespace EasyGelf.NLog
+{
+    [Target("GelfHttp")]
+    public sealed class GelfHttpTarget : GelfTargetBase
+    {
+        public string Url { get; set; }
+
+        public int Timeout { get; set; }
+
+        public int RemotePort { get; set; }
+
+        public GelfHttpTarget()
+        {
+            Url = "http://localhost";
+            Timeout = 3000;
+            RemotePort = 80;
+        }
+
+        protected override ITransport InitializeTransport(IEasyGelfLogger logger)
+        {
+            var configuration = new HttpTransportConfiguration
+            {
+                Uri = Url,
+                Timeout = 3000,
+                Port = RemotePort
+            };
+
+            return new HttpTransport(configuration, new GelfMessageSerializer());
+        }
+    }
+}
